8b7aca6e67baf3710f77fd2919b1744e8883d0c5
Vyacheslav <gracerpro@yandex.ru>
2023-12-06 23:11:31 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Hero-wars website
+
+## Как запускать
+
+Для поднятия контейнеров
+
+```bash
+docker-compose up --detach
+docker-compose up --detach --build --force-recreate
+```
+
+Для выполнения команды, без захода в контейнер
+
+```
+docker exec [контейнер] [команда]
+docker exec hero-wars_vue npm run build
+docker exec hero-wars_vue npm run lint --fix
+```
+
+Для захода в **vue** контейнер
+
+```bash
+docker exec -it --user "$(id -u):$(id -g)" hero-wars_vue bash
+```
+
+## Project setup
+
+Добавить в `/etc/hosts` на хостовом компьютере.
+
+127.0.0.1 hero-wars.local
+
+Скачать проект на компьютер. Установить зависимости
+
+```bash
+docker exec hero-wars_vue npm install
+```
---
+++ src/views/HomePage.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h1>Соберем все призы вместе!</h1>
+
+    <p v-if="creating">Loading...</p>
+    <svg
+      v-else
+      width="800"
+      height="600"
+      viewBox="-5 -5 10 10"
+      style="padding: 10px; margin: 0 auto; outline: 1px solid #ddd"
+    >
+      <circle
+        v-for="(item, index) in coordinates"
+        :key="index"
+        :cx="item[0]"
+        :cy="item[1]"
+        r="0.5"
+        stroke="black"
+        stroke-width="0.1"
+        fill="red"
+        class="circle"
+      />
+    </svg>
+
+    <p>Кликни на ячейку и впиши что в ней находится</p>
+    <ol>
+      <li>Название</li>
+      <li>Кличество</li>
+    </ol>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomePage",
+  components: {},
+  data() {
+    return {
+      creating: true,
+      coordinates: [],
+    };
+  },
+  mounted() {
+    this.createField();
+  },
+  methods: {
+    createField() {
+      this.creating = true;
+
+      const coordinates = [
+        [0, -1],
+        [0, 0],
+        [0, 1],
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ];
+
+      this.coordinates = coordinates;
+
+      this.creating = false;
+    },
+  },
+};
+</script>
+<style scoped>
+.circle:hover {
+  fill: green;
+  cursor: pointer;
+}
+</style>
