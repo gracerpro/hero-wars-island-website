7469c5bc31a9b6ae61ae0809d0b0aa7bc26ecc8e
Vyacheslav <gracerpro@yandex.ru>
2023-12-07 19:49:11 +0300

    Draw a region

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,19 +7,14 @@
       v-else
       width="800"
       height="600"
-      viewBox="-5 -5 10 10"
+      viewBox="-20 -20 40 40"
       style="padding: 10px; margin: 0 auto; outline: 1px solid #ddd"
     >
-      <circle
-        v-for="(item, index) in coordinates"
+      <polygon
+        v-for="(item, index) in paths"
         :key="index"
-        :cx="item[0]"
-        :cy="item[1]"
-        r="0.5"
-        stroke="black"
-        stroke-width="0.1"
-        fill="red"
-        class="circle"
+        :points="item.spacePaths"
+        class="node"
       />
     </svg>
 
@@ -38,6 +33,7 @@ export default {
     return {
       creating: true,
       coordinates: [],
+      paths: [],
     };
   },
   mounted() {
@@ -48,26 +44,79 @@ export default {
       this.creating = true;
 
       const coordinates = [
-        [0, -1],
+        [0, -10],
         [0, 0],
-        [0, 1],
-        [-1, -1],
-        [-1, 0],
-        [-1, 1],
-        [1, -1],
-        [1, 0],
-        [1, 1],
+        [0, 10],
+        [-10, -10],
+        [-10, 0],
+        [-10, 10],
+        [10, -10],
+        [10, 0],
+        [10, 10],
       ];
 
+      const leftNodeCount = 10;
+      const rightNodeCount = 10;
+      const topNodeCount = 5;
+      const bottomNodeCount = 5;
+
+      let paths = [];
+      const side = 4;
+      const halfSide = side / 2;
+      const h = (Math.sqrt(3) / 2) * side;
+
+      coordinates.forEach((item) => {
+        const x = item[0];
+        const y = item[1];
+
+        let arr = new Array(6);
+        console.log(arr);
+        arr[0] = { x: x + side, y: y };
+        arr[1] = { x: x + halfSide, y: y + h };
+        arr[2] = { x: x - halfSide, y: y + h };
+        arr[3] = { x: x - side, y: y };
+        arr[4] = { x: x - halfSide, y: y - h };
+        arr[5] = { x: x + halfSide, y: y - h };
+        console.log(arr);
+
+        paths.push({
+          coordinates: arr,
+          spacePaths: this.getPaths(arr),
+        });
+      });
+      console.log(paths);
+
       this.coordinates = coordinates;
+      this.paths = paths;
 
       this.creating = false;
     },
+    getPaths(coordinates) {
+      return coordinates.map((item) => item.x + "," + item.y).join(" ");
+    },
   },
 };
+/*
+<circle
+        v-for="(item, index) in coordinates"
+        :key="index"
+        :cx="item[0]"
+        :cy="item[1]"
+        r="0.5"
+        stroke="black"
+        stroke-width="0.1"
+        fill="red"
+        class="circle"
+      />
+      */
 </script>
 <style scoped>
-.circle:hover {
+.node {
+  fill: lime;
+  stroke: purple;
+  stroke-width: 0.1
+}
+.node:hover {
   fill: green;
   cursor: pointer;
 }
