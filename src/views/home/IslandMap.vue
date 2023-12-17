e52babfe8d6026fffde7fbba1ec2671cf13c5b12
Vyacheslav <gracerpro@yandex.ru>
2023-12-17 19:51:26 +0300

    Scale and translate

--- src/views/home/IslandMap.vue
+++ src/views/home/IslandMap.vue
@@ -1,91 +1,124 @@
 <template>
-  <div class="map">
+  <div>
     <div v-if="loadingNodes">Loading...</div>
-    <div v-else>
-      <svg
-        height="600"
-        width="100%"
-        :viewBox="viewBox"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <line
-          x1="0"
-          y1="-9999"
-          x2="0"
-          y2="9999"
-          style="stroke: black; stroke-width: 1"
-        />
-        <line
-          x1="-9999"
-          y1="0"
-          x2="9999"
-          y2="0"
-          style="stroke: black; stroke-width: 1"
-        />
-
-        <polygon
-          v-for="node in nodes"
-          :key="node.xyId"
-          :points="node.points"
-          class="node"
-          :class="node.class"
-          v-on:mouseenter="nodeMouseEnter(node)"
-        />
-        <template v-for="item in nodeIcons" :key="item.xyId">
-          <image
-            :x="item.x"
-            :y="item.y"
-            :width="imageSide"
-            :height="imageSide"
-            :href="item.iconUrl"
-          />
-          <text :x="item.textX" :y="item.textY" class="node-text">
-            {{ item.name }}
-          </text>
-        </template>
-      </svg>
-
-      <div style="max-width: 30em">
-        <p>Кликни на ячейку и впиши что в ней находится</p>
-        <ol>
-          <li>Название</li>
-          <li>Кличество, если больше 1</li>
-        </ol>
+    <template v-else>
+      <div class="actions">
+        <div>
+          <b>scale</b>
+          <button type="button" @click="onChangeScale(true)">+ up</button>
+          <button type="button" @click="onChangeScale(false)">- down</button>
+          <button type="button" @click="onResetScale()">reset</button>
+        </div>
+        <div>
+          <b>translate</b>
+          <button type="button" @click="onChangeTranslate(-1, 0)">left</button>
+          <button type="button" @click="onChangeTranslate(1, 0)">right</button>
+          <button type="button" @click="onChangeTranslate(0, 1)">top</button>
+          <button type="button" @click="onChangeTranslate(0, -1)">down</button>
+          <button type="button" @click="onResetTranslate()">reset</button>
+        </div>
       </div>
+      <div class="map">
+        <svg
+          height="600"
+          width="100%"
+          :viewBox="viewBox"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <line
+            x1="0"
+            y1="-9999"
+            x2="0"
+            y2="9999"
+            style="stroke: black; stroke-width: 1"
+          />
+          <line
+            x1="-9999"
+            y1="0"
+            x2="9999"
+            y2="0"
+            style="stroke: black; stroke-width: 1"
+          />
 
-      <table v-if="nodeItems.length">
-        <thead>
-          <tr>
-            <th>
-              Предмет<br />
-              <input value="" />
-            </th>
-            <th>Количество</th>
-            <th>
-              Показать на карте<br />
-              <button type="button">Сбросить</button>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="nodeItem in nodeItems" :key="nodeItem.id">
-            <td>{{ nodeItem.itemName }}</td>
-            <td>{{ nodeItem.quantity }}</td>
-            <td><input type="checkbox" value="0" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          <g
+            :transform="
+              'translate(' +
+              translateX +
+              ' ' +
+              translateY +
+              ') scale(' +
+              scaleValue +
+              ')'
+            "
+          >
+            <polygon
+              v-for="node in nodes"
+              :key="node.xyId"
+              :points="node.points"
+              class="node"
+              :class="node.class"
+              v-on:mouseenter="nodeMouseEnter(node)"
+            />
+            <template v-for="item in nodeIcons" :key="item.xyId">
+              <image
+                :x="item.x"
+                :y="item.y"
+                :width="imageSide"
+                :height="imageSide"
+                :href="item.iconUrl"
+              />
+              <text :x="item.textX" :y="item.textY" class="node-text">
+                {{ item.name }}
+              </text>
+            </template>
+          </g>
+        </svg>
+
+        <div style="max-width: 30em">
+          <p>Кликни на ячейку и впиши что в ней находится</p>
+          <ol>
+            <li>Название</li>
+            <li>Кличество, если больше 1</li>
+          </ol>
+        </div>
+
+        <table v-if="nodeItems.length">
+          <thead>
+            <tr>
+              <th>
+                Предмет<br />
+                <input value="" />
+              </th>
+              <th>Количество</th>
+              <th>
+                Показать на карте<br />
+                <button type="button">Сбросить</button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="nodeItem in nodeItems" :key="nodeItem.id">
+              <td>{{ nodeItem.itemName }}</td>
+              <td>{{ nodeItem.quantity }}</td>
+              <td><input type="checkbox" value="0" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </template>
   </div>
 </template>
 <script>
 import HeroClient, { TYPE_TOWN, TYPE_START } from "@/api/HeroClient";
 
-const SIDE = 100;
-const HALF_SIDE = 50;
-const HEIGHT = 40;
+const SIDE = 86;
+const HALF_SIDE = SIDE / 2;
+const HEIGHT = 36;
 const IMAGE_SIDE = 40;
-//const FONT_SIZE = 30;
+const FONT_SIZE = 22;
+
+const MAX_SCALE = 3;
+const MIN_SCALE = 0.3;
 
 export default {
   client: new HeroClient(),
@@ -101,6 +134,9 @@ export default {
       nodeDialogComponent: null,
       updating: false,
       nodes: [],
+      scaleValue: 1,
+      translateX: 0,
+      translateY: 0,
     };
   },
   computed: {
@@ -127,7 +163,7 @@ export default {
             itemCount === 1 ? node.x - 0.5 * HALF_SIDE : node.x - HALF_SIDE;
           let y = node.y - 0.9 * HEIGHT;
           let textX = itemCount === 1 ? node.x - 0.25 * SIDE : node.x - textCX;
-          let textY = node.y + 0.65 * HEIGHT;
+          let textY = y + IMAGE_SIDE + FONT_SIZE - 2;
 
           node.items.forEach((item, index) => {
             if (item.iconUrl && index < maxIconCount) {
@@ -218,6 +254,35 @@ export default {
         class: nodeClass,
       };
     },
+    /**
+     * @param {Boolean} inc
+     */
+    onChangeScale(inc) {
+      this.scaleValue += inc ? 0.1 : -0.1;
+
+      if (this.scaleValue > MAX_SCALE) {
+        this.scaleValue = MAX_SCALE;
+      } else if (this.scaleValue < MIN_SCALE) {
+        this.scaleValue = MIN_SCALE;
+      }
+    },
+    onResetScale() {
+      this.scaleValue = 1;
+    },
+    onChangeTranslate(dx, dy) {
+      const value = 5;
+
+      if (dx !== 0) {
+        this.translateX += dx > 0 ? value : -value;
+      }
+      if (dy !== 0) {
+        this.translateY += dy > 0 ? value : -value;
+      }
+    },
+    onResetTranslate() {
+      this.translateX = 0;
+      this.translateY = 0;
+    },
     nodeMouseEnter(node) {
       this.activeNode = node;
     },
@@ -286,5 +351,8 @@ export default {
   height: 600px;
   padding: 10px;
   outline: 1px solid #dddddd;
+}
+.actions {
+  margin-bottom: 10px;
 }
 </style>
