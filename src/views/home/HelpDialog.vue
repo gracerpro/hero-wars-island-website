18dd88b8f3a342569ea0072c843b5f5c0ad739ca
Vyacheslav <gracerpro@yandex.ru>
2024-01-06 23:12:09 +0300

    Help dialog

--- src/views/home/HelpDialog.vue
+++ src/views/home/HelpDialog.vue
@@ -2,19 +2,28 @@
   <modal-dialog
     :element-id="dialogId"
     :is-show-submit="false"
-    header="Хочешь помочь?"
+    header="Карта"
     ref="dialog"
   >
+    <p>
+      При перемещении курсора если нажать среднюю кнопку мышки - сдвиг.<br />
+      Прокручивать средней кнопкой мышки - увеличение или уменьшение.
+    </p>
+    <h4>Узлы</h4>
+    <ul>
+      <li><span class="node-box node-start"></span>Начало, вход</li>
+      <li><span class="node-box node"></span>Один ресурс</li>
+      <li><span class="node-box node-town"></span>Башня</li>
+      <li>
+        <span class="node-box node-chest"></span>Сундук, несколько ресурсов
+      </li>
+    </ul>
+    <h4>Хочешь помочь?</h4>
     <p>На ячейке со знаком вопроса нужно вписать или поправить ресурс.</p>
-    <ol>
+    <ol class="mb-0">
       <li>Название ресурса, главное понятное и существующее</li>
       <li>Количество, если больше 1 (например, 123, 300к, 5М)</li>
     </ol>
-    <h4>Карта</h4>
-    <p>При перемещении курсора если нажать среднюю кнопку мышки - сдвиг.</p>
-    <p class="mb-0">
-      Прокручивать средней кнопкой мышки - увеличение или уменьшение.
-    </p>
   </modal-dialog>
 </template>
 <script>
@@ -41,3 +50,25 @@ export default {
   },
 };
 </script>
+<style scoped>
+.node-box {
+  display: inline-block;
+  width: 2em;
+  height: 1em;
+  border: 1px solid #dddddd;
+  vertical-align: middle;
+  margin-right: 0.5em;
+}
+.node {
+  background-color: #9da7c9;
+}
+.node-start {
+  background-color: #55211d;
+}
+.node-town {
+  background-color: #94440e;
+}
+.node-chest {
+  background-color: #1a660b;
+}
+</style>
