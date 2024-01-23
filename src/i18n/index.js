3e9ca94c4af5bba491e32d1e2320fedd1ce95bc0
Vyacheslav <gracerpro@yandex.ru>
2024-01-23 12:52:33 +0300

    Translate service

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@ import { setupI18n } from "./translation";
 import ru from "./locales/ru.json";
 import en from "./locales/en.json";
 
-export default setupI18n({
+const i18n = setupI18n({
   locale: process.env.VUE_APP_DEFAULT_LOCALE,
   fallbackLocale: process.env.VUE_APP_DEFAULT_LOCALE,
   legacy: false,
@@ -10,3 +10,5 @@ export default setupI18n({
   messages: { ru, en },
   warnHtmlMessage: false,
 });
+
+export default i18n;
