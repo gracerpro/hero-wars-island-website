277008b0f23cc4ecd7c66c3eb5fa1be116167f38
Vyacheslav <gracerpro@yandex.ru>
2024-02-20 15:41:19 +0300

    Dev server

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,17 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 
-// https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
 
-  console.log(mode, env);
-
   return {
     server: {
-      port: 3000
+      host: true,
+      port: 8082
     },
     plugins: [
       vue(),
-      ViteEjsPlugin({...env}),
+      ViteEjsPlugin(env),
     ],
     resolve: {
       alias: {
@@ -24,29 +22,3 @@ export default defineConfig(({mode}) => {
     }
   }
 })
-
-/*
-chainWebpack: (config) => {
-    config.plugin("define").tap((definitions) => {
-      Object.assign(definitions[0], {
-        //__VUE_PROD_DEVTOOLS__: isDebug, // Conflicting values for '__VUE_PROD_DEVTOOLS__'
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: isDebug,
-      });
-      return definitions;
-    });
-  },
-  pages: {
-    app: {
-      entry: "src/main.js",
-      template: "public/index.html",
-      filename: "index.html",
-      chunks: ["chunk-vendors", "chunk-common", "app"],
-    },
-    swagger: {
-      entry: "src/swagger.js",
-      template: "public/backend-api/index.html",
-      filename: "backend-api/index.html",
-      chunks: ["chunk-vendors", "chunk-common", "swagger"],
-    },
-  },
-  */
