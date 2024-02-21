ae8f97a146758c9acb5f10602577479286a82cae
Vyacheslav <gracerpro@yandex.ru>
2024-02-21 11:50:23 +0300

    Add server js

---
+++ server.js
@@ -0,0 +1,119 @@
+import fs from 'node:fs/promises'
+import express from 'express'
+
+// Constants
+const isProduction = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 5173
+const base = process.env.BASE || '/'
+
+// Cached production assets
+const templateHtml = isProduction
+  ? await fs.readFile('./dist/client/index.html', 'utf-8')
+  : ''
+const ssrManifest = isProduction
+  ? await fs.readFile('./dist/client/.vite/ssr-manifest.json', 'utf-8')
+  : undefined
+
+// Create http server
+const app = express()
+
+// Add Vite or respective production middlewares
+let vite
+if (!isProduction) {
+  const { createServer } = await import('vite')
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+    base
+  })
+  app.use(vite.middlewares)
+} else {
+  const compression = (await import('compression')).default
+  const sirv = (await import('sirv')).default
+  app.use(compression())
+  app.use(base, sirv('./dist/client', { extensions: [] }))
+}
+
+// Serve HTML
+app.use('*', async (request, response) => {
+  try {
+    const url = request.originalUrl.replace(base, '')
+
+    let template
+    let render
+    if (!isProduction) {
+      // Always read fresh template in development
+      template = await fs.readFile('./index.html', 'utf-8')
+      template = await vite.transformIndexHtml(url, template)
+      render = (await vite.ssrLoadModule('/src/entry-server.js')).render
+
+
+      // 3b. Since Vite 5.1, you can use the experimental createViteRuntime API
+      //    instead.
+      //    It fully supports HMR and works in a simillar way to ssrLoadModule
+      //    More advanced use case would be creating a runtime in a separate
+      //    thread or even a different machine using ViteRuntime class
+      //const runtime = await vite.createViteRuntime(server)
+      //const { render } = await runtime.executeEntrypoint('/src/entry-server.js')
+
+    } else {
+      template = templateHtml
+      render = (await import('./dist/server/entry-server.js')).render
+    }
+
+    const rendered = await render(url, ssrManifest)
+
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? '')
+      .replace(`<!--app-html-->`, rendered.html ?? '')
+
+      response.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+  } catch (e) {
+    vite?.ssrFixStacktrace(e)
+    console.log(e.stack)
+    response.status(500).end(e.stack)
+  }
+})
+
+// server.use(express.static(path.join(rootDirectory, "/dist/client")));
+
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+});
+
+/*
+async function handleRequest(request) {
+  const context = {
+    url: request.url,
+  }
+
+  return new Promise((resolve, reject) => {
+    // Each request - new instance
+    const { app, router } = createApp();
+
+    if (!app) {
+      reject();
+    }
+
+    renderToString(app, context)
+        .then((html) => resolve(html));
+
+    if (router) {
+      router.push(context.url);
+
+      router.onReady(() => {
+        const matchedComponents = router.getMatchedComponents();
+        console.log("matchedComponents", matchedComponents);
+
+        if (!matchedComponents.length) {
+          return reject(new HttpError(404, "Page not found"));
+        }
+
+        renderToString(app, context)
+          .then((html) => resolve(html));
+      });
+    }
+  });
+}
+*/
--- src/entry-server.js
+++ src/entry-server.js
@@ -1,81 +1,15 @@
-import express from 'express'
-import fs from "fs";
-import path from "path";
 import { renderToString } from 'vue/server-renderer'
-import createApp from './main2.js';
-import HttpError from './exceptions/HttpError.js';
+import createApp from './main2'
 
-const rootDirectory = process.cwd();
-const port = 3000;
-const server = express();
+export async function render() {
+  const { app } = createApp()
 
-console.log(rootDirectory);
+  // passing SSR context object which will be available via useSSRContext()
+  // @vitejs/plugin-vue injects code into a component's setup() that registers
+  // itself on ctx.modules. After the render, ctx.modules would contain all the
+  // components that have been instantiated during this render call.
+  const context = {}
+  const html = await renderToString(app, context)
 
-server.get('/', (request, response) => {
-  handleRequest(request)
-  .then((html) => {
-    const template = fs.readFileSync(
-      path.join(rootDirectory, '/dist/index.html'),
-      "utf-8"
-    );
-    console.log("1", html);
-    html = template.replace("<!--app-html-->", html);
-    console.log("2", html);
-
-    response
-   // .setHeader("Content-Type", "text/html; charset=utf-8")
-    .status(200)
-    .send(html);
-  })
-  .catch((error) => {
-    console.error("ERROR", error);
-
-    if (error instanceof HttpError) {
-      response.status(error.statusCode).end();
-    } else {
-      response.status(500).end();
-    }
-  })
-});
-
-server.use(express.static(path.join(rootDirectory, "/dist")));
-
-server.listen(port, () => {
-  console.log(`Server started at localhost:${port}. Press Ctrl+C to quit.`)
-});
-
-
-
-async function handleRequest(request) {
-  const context = {
-    url: request.url,
-  }
-
-  return new Promise((resolve, reject) => {
-    // Each request - new instance
-    const { app, router } = createApp();
-
-    if (!app) {
-      reject();
-    }
-
-    renderToString(app, context)
-        .then((html) => resolve(html));
-
-    if (router) {
-      router.push(context.url);
-
-      router.onReady(() => {
-        const matchedComponents = router.getMatchedComponents();
-        console.log("matchedComponents", matchedComponents);
-
-        if (!matchedComponents.length) {
-          return reject(new HttpError(404, "Page not found"));
-        }
-
-        renderToString(app, context)
-          .then((html) => resolve(html));
-      });
-    }
-  });
+  return { html }
 }
