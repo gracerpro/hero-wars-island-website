7cce5656d70fa77281eb78d23ec288b4a7f48a6d
Vyacheslav <gracerpro@yandex.ru>
2024-09-20 22:51:47 +0300

    Add test for news page

--- tests/views/TheHome.test.js
+++ tests/views/TheHome.test.js
@@ -3,68 +3,9 @@ import TheHome from '../../src/views/TheHome.vue'
 import { describe, it, expect, vi } from 'vitest'
 import Island from '@/api/Island'
 import News from '@/api/News'
-
-const responseIslands = {
-  items: [
-    {
-      id: 1,
-      name: "First island",
-      eventStartAt: "2000-01-01 01:00:00",
-      eventEndAt: "2000-02-01 12:00:00",
-      nodesLastUpdatedAt: "2000-01-15 00:00:00",
-    },
-    {
-      id: 1,
-      name: "Second island",
-      eventStartAt: "2000-02-01 01:00:00",
-      eventEndAt: "2000-03-01 12:00:00",
-      nodesLastUpdatedAt: "2000-02-15 00:00:00",
-    },
-    {
-      id: 1,
-      name: "Third island",
-      eventStartAt: "2000-03-01 01:00:00",
-      eventEndAt: "2000-04-01 12:00:00",
-      nodesLastUpdatedAt: "2000-03-15 00:00:00",
-    },
-  ],
-  totalCount: 3,
-}
-
-const responseNews = {
-  items: [
-    {
-      id: 1,
-      slug: "one",
-      createdAt: new Date("2024-01-01 00:00:01"),
-      updatedAt: new Date("2024-01-01 00:00:01"),
-      name: "One",
-      snippet: "<p>One</p>"
-    },
-    {
-      id: 2,
-      slug: "two",
-      createdAt: new Date("2024-01-02 00:00:02"),
-      updatedAt: new Date("2024-01-02 00:00:02"),
-      name: "Two",
-      snippet: "<p>Two</p>"
-    },
-    {
-      id: 3,
-      slug: "three",
-      createdAt: new Date("2024-01-03 00:00:03"),
-      updatedAt: new Date("2024-01-03 00:00:03"),
-      name: "Three",
-      snippet: "<p>Three</p>"
-    },
-  ],
-  totalCount: 10
-}
-
-const responseEmptyList = {
-  items: [],
-  totalCount: 0
-}
+import { responseEmptyList } from "../data/common"
+import { responseNews } from "../data/news"
+import { responseIslands } from "../data/island"
 
 describe('The home page', () => {
   it("mount component", async () => {
