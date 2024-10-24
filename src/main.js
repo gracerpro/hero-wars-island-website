0799f99d1ebb38414e1952f81ab495547230aca3
Vyacheslav <gracerpro@yandex.ru>
2024-10-24 10:53:58 +0300

    Remove the sentry dependency

--- src/main.js
+++ src/main.js
@@ -11,10 +11,6 @@ export default function createApp() {
   const i18n = createI18n();
   const store = createStore();
 
-  if (!import.meta.env.SSR && import.meta.env.VITE_USE_SENTRY > 0) {
-    initSentry(app, router);
-  }
-
   app.use(store).use(router).use(i18n);
 
   return {
@@ -22,28 +18,4 @@ export default function createApp() {
     router,
     store,
   };
-}
-
-async function initSentry(app, router) {
-  const Sentry = await import("@sentry/vue");
-
-  Sentry.init({
-    app,
-    dsn: "https://e6453d231e745bf943c79277ccdc8890@o514031.ingest.sentry.io/4506580248297472",
-    integrations: [
-      Sentry.browserTracingIntegration({ router }),
-      Sentry.replayIntegration({
-        maskAllText: true,
-        blockAllMedia: true,
-      }),
-    ],
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ["localhost", import.meta.env.VITE_BACKEND_API_URL],
-
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
 }
