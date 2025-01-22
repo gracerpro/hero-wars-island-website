066459b7d92043bcb81340a9cd28e32e3538cbfa
Vyacheslav <gracerpro@yandex.ru>
2025-01-22 20:16:57 +0300

    Add compress assests

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { ViteEjsPlugin } from "vite-plugin-ejs";
+import compression from 'vite-plugin-compression2';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
@@ -21,6 +22,10 @@ export default defineConfig(({mode}) => {
     plugins: [
       vue(),
       ViteEjsPlugin(env),
+      compression({
+        algorithm: 'gzip',
+        exclude: [/\.(br)$ /, /\.(gz)$/]
+      }),
     ],
     resolve: {
       alias: {
